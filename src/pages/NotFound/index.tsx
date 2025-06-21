c0d8512945765a979037281c2b81ec2b6e955226
ytvee <tyanbz@gmail.com>
2025-06-21 04:30:11 +0300

    prepare for update

--- src/pages/NotFound/index.tsx
+++
@@ -1,7 +0,0 @@
-import { type ReactElement } from "react";
-
-const NotFoundPage = (): ReactElement => {
-    return <div className="not-found-page">Not Found Page</div>;
-};
-
-export default NotFoundPage;
