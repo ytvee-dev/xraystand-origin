fab9e0fdbe0a28ca6fad0d52a17505f1ffd494e7
ytvee <tyanbz@gmail.com>
2025-08-18 18:45:24 +0300

    added flexible modal playground 1

--- src/components/common/Modal/FlexibleModal/index.tsx
+++ src/components/common/Modal/FlexibleModal/index.tsx
@@ -1,13 +1,13 @@
 import {type ReactElement, type ReactNode} from "react";
+import {useSelector} from "react-redux";
+import type {TRootState} from "@store/index.ts";
 import Button from '@mui/material/Button';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import "./style.css"
 import {Languages} from "@domains/Translate";
-import {useSelector} from "react-redux";
-import type {TRootState} from "@store/index.ts";
+import "./style.css"
 
 export interface IFlexibleModal {
     modalHeader?: string;
