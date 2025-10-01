035f4014fb1063dba53c870e948b2af42b066584
ytvee <yevgeniy.tyan.v@gmail.com>
2025-10-01 19:43:00 +0300

    added brainCard

---
+++ src/modules/math/components/BrainCard/index.tsx
@@ -0,0 +1,25 @@
+import React, {type ReactElement} from "react";
+import "./style.css";
+
+const DEFAULT_BACKGROUND_COLOR = "#2769A5";
+
+export interface IBrainCardProps {
+    title: string;
+    description: string;
+    backgroundColor?: string;
+}
+
+const BrainCard: React.FC<IBrainCardProps> = ({title, description, backgroundColor=DEFAULT_BACKGROUND_COLOR}): ReactElement => {
+    // const isMobile = useScreenWidth() < 800;
+
+    return (
+        <div className={'brain-card'} style={{backgroundColor: backgroundColor}}>
+            <div className={'brain-card-content'}>
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BrainCard;
