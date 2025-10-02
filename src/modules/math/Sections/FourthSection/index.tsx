87ff96c8bc6a7cfc21c6e07d9137cb399c3d0e2c
ytvee <yevgeniy.tyan.v@gmail.com>
2025-10-02 19:08:56 +0300

    added last section

--- src/modules/math/Sections/FourthSection/index.tsx
+++ src/modules/math/Sections/FourthSection/index.tsx
@@ -1,49 +1,39 @@
 import React, {type ReactElement} from "react";
 import type {MathSectionProps} from "@modules/math/types";
-
-import "./style.css";
-import useScreenWidth from "@hooks/useScreenWidth.ts";
+import type {TRootState} from "@store/index.ts";
 import {Languages} from "@domains/Translate";
 import {useSelector} from "react-redux";
-import type {TRootState} from "@store/index.ts";
+import GridCard from "@modules/math/components/GridCard";
+import paths from "@modules/math/locales/paths.json";
 import * as textContentKz from "@modules/math/locales/kaz.json";
 import * as textContentRu from "@modules/math/locales/rus.json";
-import paths from "@modules/math/locales/paths.json";
-import CubeCard from "@modules/math/components/CubeCard";
-import type {TCard} from "@modules/math/Sections/SecondSection";
-import {getFlexDirection} from "@modules/math/Sections/FirstSection";
+import "./style.css";
 
 const FourthSection: React.FC<MathSectionProps> = ({className}: MathSectionProps): ReactElement => {
-    const isMobile = useScreenWidth() < 800;
     const currentLocale: Languages = useSelector((state: TRootState) => state.locale.locale);
     const textContent = currentLocale === Languages.KAZAKH ? textContentKz : textContentRu;
 
-    const cards: TCard[] = textContent.fourthSection.content.map((contentItem, idx) => {
-        return {
-            id: String('index-' + idx + 1),
-            title: contentItem.title,
-            subtitle: contentItem.subTitle,
-            description: contentItem.description,
-            image: paths.cards[idx] || ""
-        };
-    });
+    const cards = textContent.fourthSection.content;
 
     return (<section className={className}>
-        <div className='math-last-background-layout'>
-            <img src={paths.svg.right} alt={'image'}/>
-            {/*<img src={paths.svg.smallNet} alt={'image'}/>*/}
+        <div className='math-fourth-section-background'>
+            <img src={paths.svg.smallSpiral} alt={'background'}/>
+            <img src={paths.svg.smallSpiral} alt={'background'}/>
         </div>
-        <div className='math-cards-content'>
-            {cards.map((card, idx) => (
-                <CubeCard
-                    key={card.id}
-                    className='math-types-card'
-                    title={card.title}
-                    description={card.description}
-                    imagePath={paths.cubes[paths.cubes.length - 1 - idx]}
-                    flexDirection={card && getFlexDirection(card)}
-                />
-            ))}
+        
+        <div className='math-fourth-section-content'>
+            <h2>{textContent.fourthSection.description}</h2>
+            
+            <div className='math-cards-grid'>
+                {cards.map((card, idx) => (
+                    <GridCard
+                        key={idx}
+                        title={card.title}
+                        description={card.description}
+                        imagePath={paths.cubes[paths.cubes.length - 1 - idx]}
+                    />
+                ))}
+            </div>
         </div>
     </section>);
 };
