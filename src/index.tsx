fb46e8048fc95adf2da41a735a7bedfc25262303
ytvee <yevgeniy.tyan.v@gmail.com>
2025-10-03 15:35:18 +0300

    fixed deploy config files

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import store from "./store";
 import Application from "./components/Application";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
-        <Analytics/>
         <Application />
     </Provider>
 );
