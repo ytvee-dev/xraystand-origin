9f0275e2f96bf202bc038fa1da92ca5952eddd08
ytvee <yevgeniy.tyan.v@gmail.com>
2025-10-04 19:56:24 +0300

    structured footer content

--- src/components/common/Footers/HomeFooter/content.ts
+++ src/components/common/Footers/HomeFooter/content.ts
@@ -1,31 +1,21 @@
-import madeBySrcLight from "../../../../../public/assets/images/madeByLight.avif";
-import madeBySrcDark from "../../../../../public/assets/images/madeByDark.avif";
-import type {IDefaultDarkFooterMeta} from "./types.ts";
 
-export const trafficLawsFooterMeta: IDefaultDarkFooterMeta = {
+export const homeFooterMeta = {
     ru: {
         logoPath: `./assets/images/sprite.svg#`,
         contacts: {
-            email: "info@xraystand.kz",
-            phone: "+7 (7172) 73-50-50",
+            email: "info@edurecycle.com",
+            phone: "+7 706 624 6818",
         },
-        copyright: "© X-Ray Stand. Все права защищены",
-        content: {
-            description: "Данный продукт защищён авторским правом в соответствии " +
-                "с Законом об авторском праве и смежных правах РК. Информация " +
-                "носит ознакомительный характер.",
-            buttonsText: {
-                support: "Техподдержка",
-                privacy: "Политика конфиденциальности",
-            },
-            madeBy: {
-                link: "https://ytvee.com/service",
-                imagePath: {
-                    light: madeBySrcLight,
-                    dark: madeBySrcDark,
-                },
-            },
-        }
+        policy: 'Политика конфиденциальности',
+        year: '© 2025',
+        company: 'TOO',
+        companyLabel: '“Educational Recycle Systems”',
+        copyright: "Все права защищены",
+        madeBy: {
+            link: "https://ytvee.com/service",
+            label: 'Made by',
+            author: 'YTVEE',
+        },
     },
     kz: {
         logoPath: `./assets/images/sprite.svg#`,
@@ -33,22 +23,15 @@ export const trafficLawsFooterMeta: IDefaultDarkFooterMeta = {
             email: "info@xraystand.kz",
             phone: "+7 (7172) 73-50-50",
         },
-        copyright: "© X-Ray Stand. Барлық құқықтар қорғалған.",
-        content: {
-            description: "Осы өнім ҚР Авторлық құқық және сабақтас құқықтар " +
-                "туралы Заңына сәйкес авторлық құқықпен қорғалған. " +
-                "Ақпарат тек танысу мақсатында ұсынылған.",
-            buttonsText: {
-                support: "Техникалық қолдау",
-                privacy: "Құпиялық саясаты",
-            },
-            madeBy: {
-                link: "https://ytvee.com/service",
-                imagePath: {
-                    light: madeBySrcLight,
-                    dark: madeBySrcDark,
-                },
-            },
-        }
+        policy: 'Құпиялық саясаты',
+        year: '© 2025',
+        company: 'ЖШС',
+        companyLabel: '"Educational Recycle Systems"',
+        copyright: "Барлық құқықтар қорғалған",
+        madeBy: {
+            link: "https://ytvee.com/service",
+            label: 'Made by',
+            author: 'YTVEE',
+        },
     },
 };
