f587b1aeecda8a5068f3f983c61665b4214343e8
ytvee <yevgeniy.tyan.v@gmail.com>
2025-10-06 21:08:01 +0300

    finished lamp logic

--- src/modules/home/Sections/AboutSection/index.tsx
+++ src/modules/home/Sections/AboutSection/index.tsx
@@ -8,6 +8,21 @@ interface AboutSectionProps {
 }
 
 const AboutSection: React.FC<AboutSectionProps> = ({content}): ReactElement => {
+    const [isLightOn, setLightOn] = React.useState<boolean>(false);
+    const [currentStandsID, setCurrentStandsID] = React.useState<number[]>([4, 5]);
+
+    const handleNextImages = () => {
+        if (currentStandsID[0] === 4 && currentStandsID[1] === 5) {
+            return setCurrentStandsID([0, 1]);
+        }
+        setCurrentStandsID(
+            [
+                currentStandsID[0] + 2,
+                currentStandsID[1] + 2,
+            ]
+        );
+    }
+
     useEffect(() => {
         const toggle = document.getElementById('switch');
         if (toggle) {
@@ -15,15 +30,16 @@ const AboutSection: React.FC<AboutSectionProps> = ({content}): ReactElement => {
                 e.preventDefault();
                 e.stopPropagation();
                 this.classList.toggle('toggle-on');
+                setLightOn(prev => !prev);
             });
         }
     }, []);
 
     return (
         <section id="home-about" className="home-section about-section">
-            <div className="left-decoration">
-                <img src={'./public/assets/drops/drop_1.png'} />
-            </div>
+            {/*<div className="left-decoration">*/}
+            {/*    <img src={'./public/assets/drops/drop_1.png'} />*/}
+            {/*</div>*/}
 
             <div className="lamp-container">
                 <div className='toggle' id='switch'>
@@ -33,6 +49,12 @@ const AboutSection: React.FC<AboutSectionProps> = ({content}): ReactElement => {
                     <div className='toggle-text-on'>Вкл</div>
                 </div>
 
+                <div className='about-stands-next-button'>
+                    <button onClick={handleNextImages}>
+                        Следующий
+                    </button>
+                </div>
+
                 <div id="lamp">
                     <div className="lamp">
                         <div className="gonna-give-light"></div>
@@ -40,17 +62,21 @@ const AboutSection: React.FC<AboutSectionProps> = ({content}): ReactElement => {
                 </div>
             </div>
 
-            <div className='about-stands'>
+            <div className={`about-stands ${isLightOn ? 'light-on' : 'light-off'}`}>
                 <div className='about-stands-img-wrapper'>
-                    <img className='about-img' src={images.stands[0]} alt=''/>
-                    <img className='about-img' src={images.stands[2]} alt=''/>
-                    <img className='about-img' src={images.stands[4]} alt=''/>
+                    <img
+                        className='about-img' 
+                        src={images.stands[currentStandsID[0]]}
+                        alt='stand'
+                    />
+                    <img 
+                        className='about-img-turned'
+                        src={images.stands[currentStandsID[1]]}
+                        alt='stand'
+                        style={{ opacity: isLightOn ? 0.95 : 0 }}
+                    />
                 </div>
-                <div className='about-stands-next-button'>
-                    <button>
-                        next stand
-                    </button>
-                </div>
+
             </div>
             <div className='about-text-content'>
                 <h2>{content.title}</h2>
