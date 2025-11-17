d41b2b9c86327caf652aa51f3f776bfbffd34e76
ytvee <yevgeniy.tyan.v@gmail.com>
2025-11-17 04:29:26 +0300

    fix: analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import store from "./store";
 import Application from "./components/Application";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
