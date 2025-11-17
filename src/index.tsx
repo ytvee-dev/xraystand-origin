ca6d40e324b6aac7c1e91312b2efc269e6d445e1
ytvee <yevgeniy.tyan.v@gmail.com>
2025-11-17 04:13:31 +0300

    feat: add analitics

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import store from "./store";
 import Application from "./components/Application";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
         <Application />
+        <Analytics />
     </Provider>
 );
