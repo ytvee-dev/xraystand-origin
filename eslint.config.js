9a59f5e9e9fbc1900b9169c5d790487f8c26ad5e
ytvee <yevgeniy.tyan.v@gmail.com>
2025-12-23 00:07:29 +0300

    fix: remove linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
-// import reactConfig from "@ytvee-dev/eslint-config-react/configs/react";
-
 export default tseslint.config({ ignores: ["dist"] }, ...reactConfig, {
   plugins: {
     "react-refresh": reactRefresh,
