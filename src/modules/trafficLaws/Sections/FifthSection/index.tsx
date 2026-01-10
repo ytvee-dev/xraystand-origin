403717440bce5e8208a17fc21d16a8eba67d2615
ytvee <yevgeniy.tyan.v@gmail.com>
2026-01-10 20:37:16 +0300

    fix: update tl images in 5th section

--- src/modules/trafficLaws/Sections/FifthSection/index.tsx
+++ src/modules/trafficLaws/Sections/FifthSection/index.tsx
@@ -3,15 +3,13 @@ import type {TContentItem} from "../../types";
 import ContentSection from "../../../../components/common/Sections/DSContentSection";
 import DefaultCardsListOL from "../../components/DefaultCardsListOL";
 import DSNotification from "../../../../components/common/DSNotification";
-import usePageImagesIds from "../../../../hooks/usePageImagesIds";
-import {PageIds} from "../../../../domains/Translate";
 import {useLocaleContent} from "@hooks/useLocale";
 import * as textContentKz from "../../locales/kaz.json";
 import * as textContentRu from "../../locales/rus.json";
+import * as paths from "../../locales/paths.json";
 
 const FifthSection = (): ReactElement | null => {
     const translation = useLocaleContent(textContentRu, textContentKz);
-    const {pageImageIdData} = usePageImagesIds(PageIds.TRAFFIC_LAWS_PAGE);
 
     if (!translation.fifthSection) return null;
 
@@ -27,7 +25,7 @@ const FifthSection = (): ReactElement | null => {
             <div className={"background-cards-container"}>
                 <DefaultCardsListOL
                     content={translation.fifthSection["content"] as TContentItem[]}
-                    imageIdData={pageImageIdData?.fifthSection?.contentListData ?? []}
+                    imageIdData={paths.trafficLawsPage.fifthSection.contentListData}
                 />
             </div>
             <div className="content-section-alert-wrapper">
