51dcca405b2462eb8aabcb4c1f9ba960313f75da
RenatAllakhyarov <allakhyarovrenat@gmail.com>
2026-01-13 17:36:10 +0300

    fix: Removed too much abstraction

--- src/pages/SafetyPrecautions/index.tsx
+++ src/pages/SafetyPrecautions/index.tsx
@@ -3,8 +3,7 @@ import Spinner from "@components/common/Spinner";
 import * as paths from "@modules/safetyPrecautions/locales/paths.json";
 import * as contentRu from "@modules/safetyPrecautions/locales/rus.json";
 import * as contentKz from "@modules/safetyPrecautions/locales/kaz.json";
-import BoardSection from "@modules/safetyPrecautions/Sections/BoardSection";
-import RulesSection from "@modules/safetyPrecautions/Sections/RulesSection";
+import TwoBackgroundColorSection from "@components/common/Sections/TwoBackgroundColorSection";
 import { collectFromPathsJson } from "@utils/collectAssetUrls.ts";
 import { usePreloadImages } from "@hooks/usePreloadImages.ts";
 import { useLocaleContent } from "@hooks/useLocale";
@@ -19,151 +18,110 @@ const SafetyPrecautions = (): ReactElement => {
     const imgUrls = collectFromPathsJson(paths);
     usePreloadImages(imgUrls);
 
-    const sectionMetaTranslation = sectionMeta;
+    const {
+        commonBoardSection,
+        leftCommonRulesSection,
+        rightCommonRulesSection,
+        labsBoardSection,
+        leftLabsRulesSection,
+        rightLabsRulesSection,
+        technologyBoardSection,
+        leftTechnologyRulesSection,
+        rightTechnologyRulesSection,
+        computerBoardSection,
+        leftComputerRulesSection,
+        rightComputerRulesSection,
+    } = sectionMeta;
 
     return (
         <DefaultLayout>
             <div className="safety-precautions-page">
                 {!isContentLoaded && <Spinner />}
-                <BoardSection
-                    title={sectionMetaTranslation.commonBoardSection.title}
+                <TwoBackgroundColorSection
+                    title={commonBoardSection.title}
                     img={paths.boards.COMMON}
-                    notificationLabel={
-                        sectionMetaTranslation.commonBoardSection
-                            .notificationLabel
-                    }
+                    notificationLabel={commonBoardSection.notificationLabel}
                     backgroundLeft="#1DAA7A"
                     backgroundRight="#00855A"
                 />
                 <div className="rules">
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.leftCommonRulesSection.title
-                        }
-                        content={
-                            sectionMetaTranslation.leftCommonRulesSection
-                                .content
-                        }
+                    <TwoBackgroundColorSection
+                        title={leftCommonRulesSection.title}
+                        content={leftCommonRulesSection.content}
                         rulesBackground="#FFFFFF"
                         rulesListTitleColor="#18A778"
                         classname="left-section"
                     />
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.rightCommonRulesSection.title
-                        }
-                        content={
-                            sectionMetaTranslation.rightCommonRulesSection
-                                .content
-                        }
+                    <TwoBackgroundColorSection
+                        title={rightCommonRulesSection.title}
+                        content={rightCommonRulesSection.content}
                         rulesBackground="#246138"
                         rulesListTitleColor="#F5B423"
                         classname="right-section"
                     />
                 </div>
-                <BoardSection
-                    title={sectionMetaTranslation.labsBoardSection.title}
+                <TwoBackgroundColorSection
+                    title={labsBoardSection.title}
                     img={paths.boards.LABS}
-                    notificationLabel={
-                        sectionMetaTranslation.labsBoardSection
-                            .notificationLabel
-                    }
+                    notificationLabel={labsBoardSection.notificationLabel}
                     backgroundLeft="#886DAD"
                     backgroundRight="#6C5490"
                 />
                 <div className="rules">
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.leftLabsRulesSection.title
-                        }
-                        content={
-                            sectionMetaTranslation.leftLabsRulesSection.content
-                        }
+                    <TwoBackgroundColorSection
+                        title={leftLabsRulesSection.title}
+                        content={leftLabsRulesSection.content}
                         rulesBackground="#FFFFFF"
                         classname="left-section"
                     />
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.rightLabsRulesSection.title
-                        }
-                        content={
-                            sectionMetaTranslation.rightLabsRulesSection.content
-                        }
+                    <TwoBackgroundColorSection
+                        title={rightLabsRulesSection.title}
+                        content={rightLabsRulesSection.content}
                         rulesBackground="#4E307C"
                         rulesListTitleColor="#E66C3F"
                         classname="right-section"
                     />
                 </div>
-                <BoardSection
-                    title={sectionMetaTranslation.technologyBoardSection.title}
+                <TwoBackgroundColorSection
+                    title={technologyBoardSection.title}
                     img={paths.boards.TECHNOLOGY}
-                    notificationLabel={
-                        sectionMetaTranslation.technologyBoardSection
-                            .notificationLabel
-                    }
+                    notificationLabel={technologyBoardSection.notificationLabel}
                     backgroundLeft="#F7B626"
                     backgroundRight="#C78D0C"
                 />
                 <div className="rules">
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.leftTechnologyRulesSection
-                                .title
-                        }
-                        content={
-                            sectionMetaTranslation.leftTechnologyRulesSection
-                                .content
-                        }
+                    <TwoBackgroundColorSection
+                        title={leftTechnologyRulesSection.title}
+                        content={leftTechnologyRulesSection.content}
                         rulesBackground="#FFFFFF"
                         classname="left-section"
                     />
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.rightTechnologyRulesSection
-                                .title
-                        }
-                        content={
-                            sectionMetaTranslation.rightTechnologyRulesSection
-                                .content
-                        }
+                    <TwoBackgroundColorSection
+                        title={rightTechnologyRulesSection.title}
+                        content={rightTechnologyRulesSection.content}
                         rulesBackground="#7C5030"
                         rulesListTitleColor="#F5B423"
                         classname="right-section"
                     />
                 </div>
-                <BoardSection
-                    title={sectionMetaTranslation.computerBoardSection.title}
+                <TwoBackgroundColorSection
+                    title={computerBoardSection.title}
                     img={paths.boards.COMPUTER}
-                    notificationLabel={
-                        sectionMetaTranslation.computerBoardSection
-                            .notificationLabel
-                    }
+                    notificationLabel={computerBoardSection.notificationLabel}
                     backgroundLeft="#1D93CC"
                     backgroundRight="#0D7AB3"
                 />
                 <div className="rules">
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.leftComputerRulesSection
-                                .title
-                        }
-                        content={
-                            sectionMetaTranslation.leftComputerRulesSection
-                                .content
-                        }
+                    <TwoBackgroundColorSection
+                        title={leftComputerRulesSection.title}
+                        content={leftComputerRulesSection.content}
                         rulesBackground="#FFFFFF"
                         rulesListTitleColor="#269AD5"
                         classname="left-section"
                     />
-                    <RulesSection
-                        title={
-                            sectionMetaTranslation.rightComputerRulesSection
-                                .title
-                        }
-                        content={
-                            sectionMetaTranslation.rightComputerRulesSection
-                                .content
-                        }
+                    <TwoBackgroundColorSection
+                        title={rightComputerRulesSection.title}
+                        content={rightComputerRulesSection.content}
                         rulesBackground="#304B7C"
                         rulesListTitleColor="#F5B423"
                         classname="right-section"
