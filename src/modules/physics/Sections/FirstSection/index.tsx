0a3ce46755bfb9bad1308fa8aea9c76e6e88f93a
dkim08 <dkim191108@gmail.com>
2026-02-22 19:59:11 +0500

    mobile adaptation for cards section

--- src/modules/physics/Sections/FirstSection/index.tsx
+++ src/modules/physics/Sections/FirstSection/index.tsx
@@ -2,10 +2,10 @@ import React, { type ReactElement } from "react";
 import type { MathSectionProps } from "@modules/math/types";
 import { useLocaleContent } from "@hooks/useLocale";
 import CubeCard from "@modules/math/components/CubeCard";
+import BackgroundedTitle from "@modules/physics/components/BackgroundedTitle";
 import * as paths from "@modules/physics/locales/paths.json";
 import * as textContentKz from "@modules/physics/locales/kaz.json";
 import * as textContentRu from "@modules/physics/locales/rus.json";
-
 import "./style.css";
 
 type TPhysicsCard = {
@@ -42,10 +42,17 @@ const FirstSection: React.FC<MathSectionProps> = ({ className }: MathSectionProp
     return (
         <section className={`physics-first-section ${className || ""}`}>
             <div className="physics-first-section-content-wrapper">
-                <h2>{textContent.physicsBranchesSection.title}</h2>
-
+                <BackgroundedTitle
+                    title={textContent.physicsBranchesSection.title}
+                    description={textContent.physicsBranchesSection.description}
+                    bgColor="#ee7630"
+                    titleFontWeight="700"
+                    fullWidth
+                />
                 <div className="physics-first-section-grid">
-                    {cards.slice(0, 8).map((card) => (
+
+                    <div className="left-content-cards">
+                        {cards.slice(0, 4).map((card) => (
                         <CubeCard
                             key={card.id}
                             className="physics-types-card"
@@ -55,6 +62,20 @@ const FirstSection: React.FC<MathSectionProps> = ({ className }: MathSectionProp
                             flexDirection="row-reverse"
                         />
                     ))}
+                    </div>
+
+                    <div className="right-content-cards">
+                        {cards.slice(4, 8).map((card) => (
+                        <CubeCard
+                            key={card.id}
+                            className="physics-types-card"
+                            title={card.title}
+                            description={card.description}
+                            imagePath={card.image}
+                            flexDirection="row-reverse"
+                        />
+                    ))}
+                    </div>
                 </div>
             </div>
         </section>
