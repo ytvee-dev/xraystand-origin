a143780c735912f31eefa653fb6e08d419286fb1
ytvee <yevgeniy.tyan.v@gmail.com>
2026-03-07 21:28:06 +0300

    card example demo

--- src/pages/Implementation/index.tsx
+++ src/pages/Implementation/index.tsx
@@ -1,43 +1,36 @@
 import React from "react";
 import './style.css';
-import SquareCard from "@components/common/Cards/SquareCard";
 import SpriteIcon from "@components/common/Other/SpriteIcon";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import CardActions from "@mui/material/CardActions";
-import Button from "@mui/material/Button";
 
-const imagePath = 'https://res.cloudinary.com/dy6zg8dhs/image/upload/v1768051951/1_h2dh5q.webp';
+const imagePath = 'https://res.cloudinary.com/dy6zg8dhs/image/upload/v1771159897/card_bg_1_k5q9r9.webp';
 
-const listTempalte = [
-    'Нельзя паниковать — хаотичные действия только мешают спасению.',
-    'Нельзя возвращаться за вещами — главное сохранить жизнь.',
-    'Нельзя самостоятельно тушить крупный пожар, если пламя уже распространилось — нужно срочно эвакуироваться.',
-    'Нельзя открывать окна и двери без необходимости — это усиливает приток кислорода и раздувает пламя.',
-]
+// const listTempalte = [
+//     'Нельзя паниковать — хаотичные действия только мешают спасению.',
+//     'Нельзя возвращаться за вещами — главное сохранить жизнь.',
+//     'Нельзя самостоятельно тушить крупный пожар, если пламя уже распространилось — нужно срочно эвакуироваться.',
+//     'Нельзя открывать окна и двери без необходимости — это усиливает приток кислорода и раздувает пламя.',
+// ]
 
 const Implementation: React.FC = () => {
     return (
         <div className={"implementation-page"}>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
+            <Card className="card">
+                <CardActionArea className="card-area">
                     <CardMedia
+                        className="background-card"
                         component="img"
                         height="140"
                         image={imagePath}
                         alt="green iguana"
                     />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Lizard
-                        </Typography>
-                        
-                    </CardContent>
                 </CardActionArea>
-               
+                <div className="card-content">
+                    <SpriteIcon iconId="logo" color="white" />
+                    <p style={{color: 'white', fontWeight: 700}}>Название</p>
+                </div>
             </Card>
         </div>
     );
