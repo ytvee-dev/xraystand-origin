4e2e6f5d0bc353fbd629a50ecc4c004c4fae1d3c
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-04-02 10:20:00 +0300

    fix: Deleted test LanguageSelect

--- src/pages/English/index.tsx
+++ src/pages/English/index.tsx
@@ -1,22 +1,7 @@
-import LanguageSelect from "@components/common/Other/LanguageSelect";
 import type { ReactElement } from "react";
 
 const English = (): ReactElement => {
-    return (
-        <div
-            className="english-page"
-            style={{
-                width: "100%",
-                height: "50rem",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <LanguageSelect />
-        </div>
-    );
+    return <div className="english-page"></div>;
 };
 
 export default English;
