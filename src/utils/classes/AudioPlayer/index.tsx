85a56c22ce8dd05e2ee0095f245166b258d27942
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-04-26 17:37:30 +0300

    feat: Added class AudioPlayer

---
+++ src/utils/classes/AudioPlayer/index.tsx
@@ -0,0 +1,26 @@
+class AudioPlayer {
+    private static currentAudio: HTMLAudioElement | null;
+
+    static playAudio(src: string): Promise<void> {
+        if (this.currentAudio) {
+            this.currentAudio.pause();
+            this.currentAudio.currentTime = 0;
+            this.currentAudio = null;
+        }
+
+        const audio = new Audio(src);
+
+        this.currentAudio = audio;
+
+        return audio.play();
+    }
+
+    static stopAudio(): void {
+        if (this.currentAudio) {
+            this.currentAudio.pause();
+            this.currentAudio = null;
+        }
+    }
+}
+
+export default AudioPlayer;
