181a47ef43dc3c6e5b6b063354bd9d6e8e215e23
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-05-16 20:23:41 +0300

    fix: Changed className of the fireSafety card

--- src/modules/fireSafety/components/Card/index.tsx
+++ src/modules/fireSafety/components/Card/index.tsx
@@ -37,7 +37,7 @@ const Card: React.FC<ICardProps> = ({
                 ...style,
             }}
         >
-            <div className={"math-card-text"}>
+            <div className={"firesafety-card-text"}>
                 {title && (
                     <h3 style={{ textAlign: !isMobile ? textAlign : "center" }}>
                         {title}
