e2f491e42de3fb8d03f654b716daac5ecec2c0af
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-05-21 19:33:40 +0300

    fix: Changed ThirdSection of the SummerSection

--- src/modules/seasons/sections/SummerSection/ThirdSection/index.tsx
+++ src/modules/seasons/sections/SummerSection/ThirdSection/index.tsx
@@ -1,31 +1,37 @@
 import * as paths from "@modules/seasons/locales/paths.json";
 import ImageCard from "@modules/seasons/components/ImageCard";
 import DSNotification from "@components/common/DSNotification";
-import type { ISecondSectionProps } from "../AutumnSection/SecondSection";
+import type { ISecondSectionProps } from "../../AutumnSection/SecondSection";
 import type { SeasonsCard } from "@modules/seasons/types";
 import { SvgSpriteIds } from "@utils/constants";
 import type { ReactElement } from "react";
 import "./style.css";
 
-const NinthSection = ({ title, cards }: ISecondSectionProps): ReactElement => {
+const ThirdSection = ({ title, cards }: ISecondSectionProps): ReactElement => {
     const imgCards = cards.slice(0, 3);
 
     const blocks = cards.slice(4);
 
     return (
-        <div className="seasons-ninth-section">
-            <div className="seasons-ninth-section-content">
-                <div className="seasons-ninth-section-first-part">
+        <div className="seasons-third-section">
+            <img
+                alt=""
+                className="seasons-summer-textured-bg"
+                src="https://res.cloudinary.com/dy6zg8dhs/image/upload/v1776683839/cover_bg_1_1_vxzrdh.webp"
+            />
+
+            <div className="seasons-third-section-content">
+                <div className="seasons-third-section-first-part">
                     <h1 className="seasons-section-description">{title}</h1>
 
-                    <div className="seasons-ninth-section-blocks-container">
+                    <div className="seasons-third-section-blocks-container">
                         {imgCards.map(
                             (blockData: SeasonsCard, index: number) => (
                                 <ImageCard
                                     key={`image-card-${index}`}
-                                    id={`seasons-ninth-section-card-${index}`}
+                                    id={`seasons-third-section-card-${index}`}
                                     imagePath={paths.summer.sun}
-                                    className="seasons-ninth-section-card"
+                                    className="seasons-third-section-card"
                                     title={blockData.subTitle ?? ""}
                                     description={blockData.description ?? ""}
                                     season="summer"
@@ -35,16 +41,16 @@ const NinthSection = ({ title, cards }: ISecondSectionProps): ReactElement => {
                     </div>
                 </div>
 
-                <div className="seasons-ninth-section-second-part">
+                <div className="seasons-third-section-second-part">
                     <h1 className="seasons-section-description">
                         {cards[3].title}
                     </h1>
 
-                    <div className="seasons-ninth-section-blocks-container">
+                    <div className="seasons-third-section-blocks-container">
                         {blocks.map((blockData: SeasonsCard, index: number) => (
                             <DSNotification
                                 key={`block-${index}`}
-                                className="seasons-ninth-section-notification-block"
+                                className="seasons-third-section-notification-block"
                                 content={blockData.description}
                                 borderColor=""
                                 textColor="#FFFFFF"
@@ -71,4 +77,4 @@ const NinthSection = ({ title, cards }: ISecondSectionProps): ReactElement => {
     );
 };
 
-export default NinthSection;
+export default ThirdSection;
