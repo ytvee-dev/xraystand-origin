92ba280c94657649e6528caf4ce35abe400b01d4
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-05-28 22:19:40 +0300

    feat: Added SecondSection

--- src/modules/seasons/sections/WinterSection/index.tsx
+++ src/modules/seasons/sections/WinterSection/index.tsx
@@ -1,4 +1,5 @@
 import FirstSection from "./FirstSection";
+import SecondSection from "./SecondSection";
 import { type SeasonsProps } from "@modules/seasons/types";
 import { type ReactElement } from "react";
 import "./style.css";
@@ -7,8 +8,7 @@ const WinterSection = ({ content }: SeasonsProps): ReactElement => {
     const cards = content.content;
 
     const firstSectionCards = cards.slice(0, 4);
-    // const secondSectionCards = cards.slice(5, 8);
-    // const thirdSectionCards = cards.slice(9);
+    const secondSectionCards = cards.slice(5, 8);
 
     return (
         <section className="seasons-winter-sections-bg">
@@ -16,6 +16,11 @@ const WinterSection = ({ content }: SeasonsProps): ReactElement => {
                 title={content.title}
                 description={content.description}
                 content={firstSectionCards}
+            />
+            <SecondSection
+                title=""
+                description={cards[4].title}
+                content={secondSectionCards}
             />
         </section>
     );
