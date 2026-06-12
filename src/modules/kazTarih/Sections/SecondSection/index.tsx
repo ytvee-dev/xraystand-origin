37e3cf9ced9f637793c9f74c1d4f58d22e5ff953
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-06-12 10:18:49 +0300

    fix: Fixed all bugs

--- src/modules/kazTarih/Sections/SecondSection/index.tsx
+++ src/modules/kazTarih/Sections/SecondSection/index.tsx
@@ -1,32 +1,51 @@
 import * as paths from "@modules/kazTarih/locales/paths.json";
-import { type IKazTarihProps } from '@modules/kazTarih/types/index';
+import { type IKazTarihProps } from "@modules/kazTarih/types/index";
 import Carousel from "@modules/kazTarih/components/Carousel";
 import TextFormatterCard from "@modules/kazTarih/components/TextFormatterCard";
-import DSNotification from '@components/common/DSNotification';
+import DSNotification from "@components/common/DSNotification";
 import { type ReactElement } from "react";
 import "./style.css";
 
-const Second = ({content, summaryCardFontSize}: IKazTarihProps): ReactElement => {
+const Second = ({
+    content,
+    summaryCardFontSize,
+}: IKazTarihProps): ReactElement => {
     const cards = content.cards;
     const summaryCard = content.summaryCard;
 
     return (
         <section className="kt-second-section">
-            <img src={paths.bg} alt="" className="kt-bg-img kt-bg-light-img"/>
-            <img src={paths.lambs} alt="" className="kt-bg-lambs kt-top-lambs"/>
-            <img src={paths.lambs} alt="" className="kt-bg-lambs kt-bottom-lambs"/>
+            <img src={paths.bg} alt="" className="kt-bg-img kt-bg-light-img" />
+            <img
+                src={paths.lambs}
+                alt=""
+                className="kt-bg-lambs kt-top-lambs"
+            />
+            <img
+                src={paths.lambs}
+                alt=""
+                className="kt-bg-lambs kt-bottom-lambs"
+            />
 
             <div className="kt-second-section-content-wrapper">
-                <h2 className="kt-title gr-font-family kt-second-section-title">{content.title}</h2>
-                <p className="kt-description kt-second-section-description">{content.description}</p>
+                <h2 className="kt-title gr-font-family kt-second-section-title">
+                    {content.title}
+                </h2>
+                <p className="kt-description kt-second-section-description">
+                    {content.description}
+                </p>
 
-                <Carousel className="kt-formatter-card-wrapper">
-                    {cards.map((card, index) =>
-                        <TextFormatterCard items={card} key={index} className="kt-formatter-card"/>
-                    )}
+                <Carousel style={{ padding: "0 1rem 0 0" }}>
+                    {cards.map((card, index) => (
+                        <TextFormatterCard
+                            items={card}
+                            key={index}
+                            className="kt-formatter-card kaz-tarih-carousel-card"
+                        />
+                    ))}
                 </Carousel>
                 <div className="kt-summary-card-wrapper">
-                    <DSNotification 
+                    <DSNotification
                         content={summaryCard}
                         type="info"
                         iconColor="white"
--- src/modules/kazTarih/Sections/SixthSection/index.tsx
+++ src/modules/kazTarih/Sections/SixthSection/index.tsx
@@ -1,30 +1,42 @@
 import * as paths from "@modules/kazTarih/locales/paths.json";
-import { type IKazTarihProps } from '@modules/kazTarih/types/index';
+import { type IKazTarihProps } from "@modules/kazTarih/types/index";
 import Carousel from "@modules/kazTarih/components/Carousel";
 import TextFormatterCard from "@modules/kazTarih/components/TextFormatterCard";
-import DSNotification from '@components/common/DSNotification';
+import DSNotification from "@components/common/DSNotification";
 import { type ReactElement } from "react";
 import "./style.css";
 
-const SixthSection = ({content, summaryCardFontSize}: IKazTarihProps): ReactElement => {
+const SixthSection = ({
+    content,
+    summaryCardFontSize,
+}: IKazTarihProps): ReactElement => {
     const cards = content.cards;
     const summaryCard = content.summaryCard;
 
     return (
         <section className="kt-sixth-section">
-            <img src={paths.bg} alt="" className="kt-bg-img kt-bg-dark-img"/>
+            <img src={paths.bg} alt="" className="kt-bg-img kt-bg-dark-img" />
 
             <div className="kt-sixth-section-content-wrapper">
-                <h2 className="kt-title gr-font-family kt-sixth-section-title">{content.title}</h2>
-                <p className="kt-description kt-sixth-section-description">{content.description}</p>
+                <h2 className="kt-title gr-font-family kt-sixth-section-title">
+                    {content.title}
+                </h2>
+                <p className="kt-description kt-sixth-section-description">
+                    {content.description}
+                </p>
 
-                <Carousel className="kt-formatter-card-wrapper">
-                    {cards.map((card, index) =>
-                        <TextFormatterCard items={card} theme="dark" key={index} className="kt-formatter-card"/>
-                    )}
+                <Carousel style={{ padding: "0 1rem 0 0" }}>
+                    {cards.map((card, index) => (
+                        <TextFormatterCard
+                            items={card}
+                            theme="dark"
+                            key={index}
+                            className="kt-formatter-card kaz-tarih-carousel-card"
+                        />
+                    ))}
                 </Carousel>
                 <div className="kt-summary-card-wrapper">
-                    <DSNotification 
+                    <DSNotification
                         content={summaryCard}
                         type="info"
                         iconColor="#61361E"
