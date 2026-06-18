ae9171867d581fa20353237309999b5153fcaa70
Stephan-Petrovich <ribinstepan10@mail.ru>
2026-06-18 13:53:16 +0300

    feat: Added uncompleted CoverSection

--- src/pages/NVP/index.tsx
+++ src/pages/NVP/index.tsx
@@ -1,21 +1,22 @@
 import Spinner from "@components/common/Spinner";
 import BrightnessLayout from "@layout/Brightness";
 import * as paths from "@modules/nvp/locales/paths.json";
-// import * as contentRu from "@modules/nvp/locales/rus.json";
-// import * as contentKz from "@modules/nvp/locales/kaz.json";
+import * as contentRu from "@modules/nvp/locales/rus.json";
+import * as contentKz from "@modules/nvp/locales/kaz.json";
+import CoverSection from "@modules/nvp/Sections/CoverSection";
 import { collectFromPathsJson } from "@utils/collectAssetUrls";
 import { usePreloadImages } from "@hooks/usePreloadImages";
-// import { useLocaleContent } from "@hooks/useLocale";
+import { useLocaleContent } from "@hooks/useLocale";
 import type { TRootState } from "@store/index";
 import { useSelector } from "react-redux";
 import "./style.css";
 
 const MainLogo = () => {
-    return <div className="nvp-logo">Гражданская безопасность (ОБЖ)</div>;
+    return <div className="nvp-logo">НАЧАЛЬНАЯ ВОЕННАЯ ПОДГОТОВКА</div>;
 };
 
 const NVP: React.FC = () => {
-    // const textContent = useLocaleContent(contentRu, contentKz);
+    const textContent = useLocaleContent(contentRu, contentKz);
 
     const imgUrls = collectFromPathsJson(paths);
     usePreloadImages(imgUrls);
@@ -27,12 +28,16 @@ const NVP: React.FC = () => {
     return (
         <div className="nvp-container">
             <BrightnessLayout
-                logo={<MainLogo />}
                 isLanguageSwitcher={true}
-                headerBackgroundColor=""
+                logo={<MainLogo />}
+                headerBackgroundColor="#697855"
                 languageSwitcherClassName="nvp-switcher"
             >
                 {!isContentLoaded && <Spinner />}
+                <CoverSection
+                    className="nvp-cover-section"
+                    content={textContent.coverSection}
+                />
             </BrightnessLayout>
         </div>
     );
