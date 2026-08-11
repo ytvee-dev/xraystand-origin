dab04905afbf58e629e67f5cc3fd382d433faf8c
dianka <109206311+dkim08@users.noreply.github.com>
2026-08-12 02:32:48 +0500

    add cover section

---
+++ src/pages/AiProfession/index.tsx
@@ -0,0 +1,61 @@
+import Spinner from "@components/common/Spinner";
+import SpriteIcon from "@components/common/Other/SpriteIcon";
+import * as paths from "../../modules/aiProfession/locales/path.json";
+import * as contentRu from "../../modules/aiProfession/locales/rus.json";
+import * as contentKz from "../../modules/aiProfession/locales/kaz.json";
+import { useLocaleContent } from "@hooks/useLocale";
+import { collectFromPathsJson } from "@utils/collectAssetUrls";
+import { usePreloadImages } from "@hooks/usePreloadImages";
+import { useSelector } from "react-redux";
+import type { TRootState } from "@store/index";
+import BrightnessLayout from "@layout/Brightness";
+import CoverSection from "../../modules/aiProfession/Sections/CoverSection";
+import "./style.css";
+
+const pageLayoutSX = {
+    backgroundColor: "#9F1A4C",
+    boldTextColor: "#FFFFFF",
+    textColor: "#FFFFFF",
+    copyrightColor: "#FFFFFF",
+    logoColorMode: "light",
+    madeByColorMode: "light",
+};
+
+const MainLogo = () => {
+    return (
+        <div className="ai-profession-logo">
+            <SpriteIcon
+                iconId="logo-color"
+                width="100%"
+                height="100%"
+                color="#ffffff"
+            />
+        </div>
+    );
+};
+
+const AiProfession = () => {
+    const textContent = useLocaleContent(contentRu, contentKz);
+
+    const imgUrls = collectFromPathsJson(paths);
+    usePreloadImages(imgUrls);
+
+    const isContentLoaded: boolean = useSelector(
+        (state: TRootState) => state.application.isContentLoaded,
+    );
+    return (
+        <BrightnessLayout
+            logo={<MainLogo />}
+            stickyHeader={true}
+            isLanguageSwitcher={true}
+            switcherColor="#FFFFFF"
+            sx={pageLayoutSX}
+            headerBackgroundColor="transparent"
+        >
+            {!isContentLoaded && <Spinner />}
+            <CoverSection content={textContent.coverSection} />
+        </BrightnessLayout>
+    );
+};
+
+export default AiProfession;
