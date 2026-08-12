8b925ce148caaf88794fb44028423f5641a5d5bd
dianka <109206311+dkim08@users.noreply.github.com>
2026-08-13 01:46:03 +0500

    add mobile adaptation

--- src/modules/aiProfession/Sections/FirstSection/index.tsx
+++ src/modules/aiProfession/Sections/FirstSection/index.tsx
@@ -1,4 +1,6 @@
 import React, { type ReactElement } from "react";
+import MethodsCards from "../components/MethodCards";
+import TheoryCards from "../components/TheoryCards";
 import * as paths from "../../locales/path.json";
 import "./style.css";
 
@@ -22,11 +24,7 @@ interface FirstSectionProps {
     content: ContentItem[][];
 }
 
-const FirstSection: React.FC<FirstSectionProps> = ({title, subtitle, content}): ReactElement => {
-    const methodImages = [
-        paths.cards.firstSection[0],
-        paths.cards.firstSection[1],
-    ];
+const FirstSection: React.FC<FirstSectionProps> = ({ title, subtitle, content,}): ReactElement => {
 
     return (
         <section className="ai-profession-first-section">
@@ -38,98 +36,9 @@ const FirstSection: React.FC<FirstSectionProps> = ({title, subtitle, content}): 
                 <h2 className="ai-profession-first-section-subtitle">{subtitle}</h2>
             </div>
 
-            <div className="ai-profession-first-section-theory">
-                <h2 className="ai-profession-first-section-theory-title">
-                    {content[0]?.[0]?.title}
-                </h2>
-
-                <div className="theory-cards">
-                    {content[0]?.slice(1).map((item, index) => {
-                        const [cardTitle, cardText] = item.description?.split("\n") ?? [];
-
-                        return (
-                            <div className="theory-card" key={index}>
-                                {cardTitle && (
-                                    <h3 className="theory-card-title">
-                                        {cardTitle}
-                                    </h3>
-                                )}
-
-                                {cardText && (
-                                    <p className="theory-card-description">
-                                        {cardText}
-                                    </p>
-                                )}
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-
-
-            <div className="ai-profession-methods">
-                {content.slice(1).map((method, index) => {
-                    const item = method[0];
-
-                    if (!item) {
-                        return null;
-                    }
-
-                    return (
-                        <div className={`ai-profession-method ai-profession-method-${index}`} key={index}>
-                            {item.title && (
-                                <h2 className="ai-profession-method-title">
-                                    {item.title}
-                                </h2>
-                            )}
-
-                            <div className={`ai-profession-method-content ${ index === 0 ? "image-left" : "image-right" }`}>
-                                {methodImages[index] && (
-                                    <img className={`ai-profession-method-image method-image-${index}`} src={methodImages[index]} alt=""/>
-                                )}
-
-                                <div className="ai-profession-method-text">
-                                    {item.subTitle && (
-                                        <p className="ai-profession-method-subtitle">{item.subTitle}</p>
-                                    )}
-
-                                    {item.description && (
-                                        <p className="ai-profession-method-description">{item.description}</p>
-                                    )}
-
-                                    {item.secondDescription && (
-                                        <p className="ai-profession-method-second-description">{item.secondDescription}</p>
-                                    )}
-
-                                    {item.list && item.list.length > 0 && (
-                                        <ol className="ai-profession-method-list">
-                                            {item.list.map(
-                                                (listItem, listIndex) => (
-                                                    <li className="ai-profession-method-list-item" key={listIndex}>
-                                                        <strong className="ai-profession-method-list-title">
-                                                            {listItem.title}
-                                                        </strong>
-
-                                                        <span className="ai-profession-method-list-description">
-                                                            ({listItem.description})
-                                                        </span>
-                                                    </li>
-                                                ),
-                                            )}
-                                        </ol>
-                                    )}
-
-                                    {item.finalDescription && (
-                                        <p className="ai-profession-method-final-description">
-                                            {item.finalDescription}
-                                        </p>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
+            
+            <TheoryCards content={content[0]} />
+            <MethodsCards content={content.slice(1)} />
         </section>
     );
 };
