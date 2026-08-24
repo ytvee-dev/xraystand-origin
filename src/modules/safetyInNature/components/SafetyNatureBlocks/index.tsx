abf470b0448717a9b2382ea2330a22c1870c0295
dianka <109206311+dkim08@users.noreply.github.com>
2026-08-24 22:51:54 +0500

    add saffety nature blocks

---
+++ src/modules/safetyInNature/components/SafetyNatureBlocks/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { type ReactElement } from "react";
+import SpriteIcon from "@components/common/Other/SpriteIcon";
+import "./style.css";
+
+export interface ISafetyNatureBlockItem {
+    title: string;
+    description: string;
+}
+
+interface ISafetyNatureBlocksProps {
+    title: string;
+    content: ISafetyNatureBlockItem[];
+    iconId: string[];
+    images: string[];
+    contentSide: "left" | "right";
+    backgroundColor?: string;
+    titleBackgroundColor?: string;
+}
+
+const SafetyNatureBlocks: React.FC<ISafetyNatureBlocksProps> = ({
+    title,
+    content,
+    iconId,
+    images,
+    contentSide,
+    backgroundColor,
+    titleBackgroundColor,
+}): ReactElement => {
+    return (
+        <div className={`safety-nature-block safety-nature-block--${contentSide}`} style={{ backgroundColor }}>
+            <h2 className="safety-nature-block-title" style={{ backgroundColor: titleBackgroundColor }}>{title}</h2>
+
+            <div className="safety-nature-block-content">
+                <div className="safety-nature-block-items">
+                    {content.map((item, index) => (
+                        <div className="safety-nature-block-item" key={item.title}>
+                            <SpriteIcon
+                                iconId={iconId[index]}
+                                width="120px"
+                                height="120px"
+                            />
+
+                            <div className="safety-nature-block-text">
+                                <h3 className="safety-nature-block-item-title">{item.title}</h3>
+                                <p className="safety-nature-block-item-description">{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="safety-nature-block-images">
+                    {images.map((image) => (
+                        <img className="safety-nature-block-image safety-nature-block-image" key={image} src={image} alt=""/>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SafetyNatureBlocks;
