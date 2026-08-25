f80daa628b16f223ea9fde10583994735a20d084
dor <heilytsukiko@gmail.com>
2026-08-26 00:02:36 +0500

    feat: add TwoImgCards

---
+++ src/modules/safetyInNature/components/TwoImgCards/index.tsx
@@ -0,0 +1,38 @@
+import type { ReactElement } from "react";
+import "./style.css"
+
+interface ITwoImgCardsContent {
+    highlightedText: string;
+    text: string;
+}
+
+interface ITwoImgCardsProps {
+    title: string;
+    content: ITwoImgCardsContent[];
+    firstImg: string;
+    secondImg: string;
+}
+
+const TwoImgCards = ({ title, content, firstImg, secondImg }: ITwoImgCardsProps): ReactElement => {
+    return (
+        <div className="two-img-cards-wrapper">
+            <p className="two-img-cards-title">{title}</p>
+            
+            <ul className="two-img-cards-list">
+                {content.map(task => (
+                    <li key={task.highlightedText} className="two-img-cards-list">
+                        <p className="two-img-cards-highlighted-text">{task.highlightedText}</p>
+                        <p className="two-img-cards-text">{task.text}</p>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="two-img-cards-images-wrapper">
+                <img src={firstImg} alt="" className="two-img-cards-first-img"/>
+                <img src={secondImg} alt="" className="two-img-cards-second-img"/>
+            </div>
+        </div>
+    )
+}
+
+export default TwoImgCards;
