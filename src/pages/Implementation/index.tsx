6201b21e0aa8c2d5b3a9bf61c58aa00d5c2a7e59
dianka <109206311+dkim08@users.noreply.github.com>
2026-08-31 16:12:48 +0500

    add infoBlock

--- src/pages/Implementation/index.tsx
+++ src/pages/Implementation/index.tsx
@@ -1,10 +1,10 @@
 import "./style.css";
-import * as paths from "../../modules/safetyInNature/locales/paths.json";
-import * as contentKz from "../../modules/safetyInNature/locales/kaz.json";
-import * as contentRu from "../../modules/safetyInNature/locales/rus.json"
+import * as paths from "../../modules/aiSecurity/locales/path.json";
+import * as contentKz from "../../modules/aiSecurity/locales/kaz.json";
+import * as contentRu from "../../modules/aiSecurity/locales/rus.json"
 import { useLocaleContent } from "@hooks/useLocale";
 import BrightnessLayout from "@layout/Brightness";
-import SafetyNatureBlocks from "@modules/safetyInNature/components/SafetyNatureBlocks";
+import InfoBlock from "@modules/aiSecurity/components/InfoBlock";
 
 const pageLayoutSX = {
     backgroundColor: "#181818",
@@ -21,63 +21,6 @@ const MainLogo = () => {
     );
 };
 
-const parksIcons = [
-    "natureSafetyTicks",
-    "natureSafetyAnimalInfections",
-    "natureSafetyQuiet",
-    "natureSafetyInfrastructure",
-];
-
-const reserveIcons = [
-    "natureSafetyTrailRule",
-    "natureSafetyDrone",
-    "natureSafetyWildlifeDistance",
-    "natureSafetyMicroclimate",
-];
-
-const waterIcons = [
-    "natureSafetyConcreteBanks",
-    "natureSafetyCrossing",
-    "natureSafetyWaterRise",
-    "natureSafetySwimming",
-];
-
-const parksImages = [
-    paths.cards.firstPresidentPark,
-    paths.cards.botanicalGargen,
-];
-
-const reserveImages = [
-    paths.cards.almatyPreacher,
-    paths.cards.ileAlatau,
-];
-
-const waterImages = [
-    paths.cards.terrenkur,
-    paths.cards.esentay,
-];
-
-interface ISafetyNatureBlockItem {
-    title: string;
-    description: string;
-}
-
-interface ISafetyNatureSectionContent {
-    title: string;
-    content: ISafetyNatureBlockItem[];
-}
-
-interface ISafetyInNatureContent {
-    parksSection: ISafetyNatureSectionContent;
-    reserveSection: ISafetyNatureSectionContent;
-    waterSection: ISafetyNatureSectionContent;
-}
-
-interface ISafetyInNatureProps {
-    content: ISafetyInNatureContent;
-}
-
-
 const SeasonPage = () => {
     const textContent = useLocaleContent(contentRu, contentKz);
 
@@ -91,34 +34,10 @@ const SeasonPage = () => {
             languageSwitcherClassName="kaz-music-language-switcher"
         >
             <div className="container">
-                <SafetyNatureBlocks
-                    title={textContent.firstSection.cards[0].title}
-                    content={textContent.firstSection.cards[0].content}
-                    iconId={parksIcons}
-                    images={parksImages}
-                    contentSide="left"
-                    backgroundColor="#77BA8FB2"
-                    titleBackgroundColor="#9CCFAE"
-                />
-
-                <SafetyNatureBlocks
-                    title={textContent.firstSection.cards[1].title}
-                    content={textContent.firstSection.cards[1].content}
-                    iconId={reserveIcons}
-                    images={reserveImages}
-                    contentSide="right"
-                    backgroundColor="#4DAC90B2"
-                    titleBackgroundColor="#6CC2B4"
-                />
-
-                <SafetyNatureBlocks
-                    title={textContent.firstSection.cards[2].title}
-                    content={textContent.firstSection.cards[2].content}
-                    iconId={waterIcons}
-                    images={waterImages}
-                    contentSide="left"
-                    backgroundColor="#348F91B2"
-                    titleBackgroundColor="#18AFAE"
+                <InfoBlock
+                    title={textContent.secondSection.topics[0].lawsCard.title}
+                    description={textContent.secondSection.topics[0].lawsCard.description}
+                    image={paths.infoBlockImages.scales}
                 />
             </div>
 
